8476c46ceff67b8d8fbf8ea0e317a729a8e3b908
unknown <saniyakhan7019@gmail.com>
2025-10-29 17:27:12 +0530

    fix: remove lightningcss issue and use tailwind v3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   fontFamily: {
+//   urbanist: ['Urbanist', 'sans-serif'],
+// },
+
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  fontFamily: {
-  urbanist: ['Urbanist', 'sans-serif'],
-},
-
+  plugins: [react()],
 })
